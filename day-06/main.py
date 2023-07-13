332b3465cc7e72b5addad17bcb450710318a0670
Md: Mohon <mdmohon6145@gmail.com>
2023-07-13 10:14:24 +0600

    practice core concept of day 6, 7, 8

---
+++ day-06/main.py
@@ -0,0 +1,30 @@
+a = "this is a Test Variable"
+b = 5
+b1 = 10
+# print(b1 + b)  
+
+x = 1
+y = True
+z= "Mohon"
+g = None
+print(" Type of x is", type(x))
+print(" Type of y is", type(y))
+print(" Type of z is", type(z))
+print(" Type of g is", type(g))
+
+complexType = complex(8, 2);
+print(complexType)
+
+list1 = [1, 2, 3, [-4, 5], ["apples", "banana"]]
+
+print(list1)
+
+tuple1 = [("parrot", "sparrow"), ("Lion", "Tiger")]
+print(tuple1)
+
+dist1 = {
+    "name": "Md. Mohon",
+    "age": 20,
+    "position": "Software Developer",
+}
+print(dist1)
---
+++ day-08/main.py
@@ -0,0 +1,6 @@
+a = 100
+b = 50
+print("The Value of ", a, " + ", b, "is: ", a + b)
+print("The Value of ", a, " + ", b, "is: ", a - b)
+print("The Value of ", a, " + ", b, "is: ", a * b)
+print("The Value of ", a, " + ", b, "is: ", a / b)
