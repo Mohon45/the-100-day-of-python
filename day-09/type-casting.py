7c7c972e08bab122dfd6fa4f81f23da83fc4e1bf
Md: Mohon <mdmohon6145@gmail.com>
2023-07-13 10:36:03 +0600

    day 9 complete

---
+++ day-09/type-casting.py
@@ -0,0 +1,14 @@
+a = "1"
+b = "2"
+# regular addition
+print(a + b)
+# type casting
+print(int(a) + int(b))
+
+# testing concation in my name like Md. Mohon
+print ("Md." + "Mohon")
+
+# implicit type casting
+c = 1.9
+d = 8
+print(c + d)
